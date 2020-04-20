472d44fce661db5c727d0ee3d997d267b1ff21ef
mais <mais.shaqbouah@gmail.com>
2020-04-21 00:13:49 +0300

    update on local repository

--- src/InvoiceTest.java
+++ src/InvoiceTest.java
@@ -4,7 +4,7 @@ public class InvoiceTest {
 
 
     public static void main (String[]args){
-
+        System.out.println("new change for local repository ");
         Invoice invoice = new Invoice("1","mais",12,20);
 
        double invoiceamount= invoice.get_Invoice_Amount();
